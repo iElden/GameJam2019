36f93b185179a8f2d9e9e83e0c463e4158a0bb00
Zargith <stanislas.hegron@gmail.com>
2019-01-26 13:00:52 +0100

    Add dummies for each tower

---
+++ include/CookingGrandMa.hpp
@@ -0,0 +1,17 @@
+//
+// Created by stan4 on 26/01/2019.
+//
+
+#ifndef GRANDMATD_COOKINGGRANDMA_HPP
+#define GRANDMATD_COOKINGGRANDMA_HPP
+
+#include "ProdTower.hpp"
+
+namespace gtd {
+	class CookingGrandMa : public ProdTower {
+	public:
+		CookingGrandMa(const sf::SoundBuffer &sBuffer, const sf::Vector2u &pos);
+	};
+}
+
+#endif //GRANDMATD_COOKINGGRANDMA_HPP
