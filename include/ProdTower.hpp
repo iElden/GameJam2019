4b5d38014e840c22c8a4191f24aa7879b46f36f9
Gegel85 <Gegel85@laposte.net>
2019-01-26 10:08:37 +0100

    Added prodtower contructor and methods

--- include/ProdTower.hpp
+++ include/ProdTower.hpp
@@ -12,12 +12,24 @@
 namespace gtd {
 	class ProdTower : public gtd::Tower {
 	protected:
+		double		_buffer;
 		gtd::Food::Type	_prodType;
 		double		_prodSpeed;
 		int		_value;
 
 	public:
-		ProdTower(const gtd::Food::Type &type, const double &speed, const int &value);
+		ProdTower(const gtd::Food::Type &type,
+			  const double &speed,
+			  const int &value,
+			  const unsigned &cost,
+			  const sf::SoundBuffer &sBuffer,
+			  const gtd::Sprite &sprite,
+			  const sf::Vector2u &pos,
+			  const double &displayedRange,
+			  const std::string &name = "Tower");
+		void		resetBuffs() override;
+		void		buff(const double &as, const double &dmg, const double &range) override;
+		virtual void	update(const float &timeSpent) override;
 		virtual	void	prod(const float &seconds);
 	};
 }
--- src/ProdTower.cpp
+++ src/ProdTower.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "ProdTower.hpp"
+
+gtd::ProdTower::ProdTower(const gtd::Food::Type &type,
+			  const double &speed,
+			  const int &value,
+			  const unsigned &cost,
+			  const sf::SoundBuffer &sBuffer,
+			  const gtd::Sprite &sprite,
+			  const sf::Vector2u &pos,
+			  const double &displayedRange,
+			  const std::string &name) :
+	gtd::Tower(cost, gtd::Tower::Production, sBuffer, sprite, pos, displayedRange, name),
+	_prodType(type),
+	_prodSpeed(speed),
+	_value(value)
+{
+}
+
+void	gtd::ProdTower::prod(const float &seconds)
+{
+	if (this->_buffer <= this->_prodSpeed)
+		this->_buffer = 0;
+	else
+		return;
+}
+
+void	gtd::ProdTower::update(const float &timeSpent)
+{
+	this->_buffer += timeSpent;
+}
